258cdf6d8f9cfe4b6c9309b7bd5cbd05b2ffcad8
Roman Rubinshteyn <romanrubbk96@mail.ru>
2022-04-20 18:43:53 +0300

    my first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+let express = require('express');
+let app = express();
+require('dotenv').config();
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const bodyParser = require('body-parser');
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'Todo list API ',
+            description: 'The description of todolist',
+            contacts: {
+                name: 'Roman Rubinshteyn'
+            },
+            servers: ['http://localhost:3000/'],
+            version: '1.0.0'
+        }
+    },
+    apis: ['./routes/*.js']
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+
+app.use(bodyParser.json());
+
+const routes = require("./routes/index");
+app.use("/api", routes);
+
+const server = app.listen(process.env.PORT, () => console.log('Server started'));
+
+module.exports = server;
